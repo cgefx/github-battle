ed603f1918429e013960cede45dbc0d7e35839e3
Colin A. Gibson <45520511+cgefx@users.noreply.github.com>
2020-05-05 12:39:46 -0700

    add tooltip component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -9,6 +9,7 @@ import {
 	FaExclamationTriangle,
 } from 'react-icons/fa';
 import Loading from './Loading';
+import Tooltip from './Tooltip';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
 	const languages = [
@@ -70,8 +71,12 @@ function ReposGrid({ repos }) {
 						>
 							<ul className='card-list'>
 								<li>
-									<FaUser color='rgb(255,191,116)' size={22} />
-									<a href={`https://github.com/${login}`}>{login}</a>
+									<Tooltip text='Github username'>
+										<FaUser color='rgb(255,191,116)' size={22} />
+										<a href={`https://github.com/${login}`}>
+											{login}
+										</a>
+									</Tooltip>
 								</li>
 								<li>
 									<FaStar color='rgb(255, 215, 0)' size={22} />
